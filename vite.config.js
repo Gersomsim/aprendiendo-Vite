4545c9ebffdde3a371646462b392181c81e22734
Gersomsim <gersomhernandez@me.com>
2023-01-21 14:03:56 -0600

    :zap: multipages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path';
+
 
 // export default defineConfig({
 //   server:{
@@ -7,12 +9,33 @@ import { defineConfig } from 'vite'
 // });
 
 
-export default defineConfig(()=>{
+export default defineConfig(({ command, mode })=>{
   const port = 3000;
 
-  return {
-    server: {
-      port
+  console.log(command, mode);
+  const env = loadEnv(mode, process.cwd())
+
+  console.log(env.VITE_NAME);
+
+  if( mode === 'development'){
+    return {
+      server: {
+        port
+      }
+    }
+  }else{
+    console.log('En modo Produccion');
+    return {
+      build: {
+        rollupOptions: {
+          input:{
+            main: resolve(__dirname, 'index.html'),
+            help: resolve(__dirname, 'help', 'help.html')
+          }
+        }
+      }
     }
   }
+
+  
 })
