efc385f7cf820a1e54566acce46fd7d9f2b71784
Gersomsim <gersomhernandez@me.com>
2023-01-21 14:36:02 -0600

    :art: cxonfiguracion para una libreria

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
-import { resolve } from 'path';
+import { format, resolve } from 'path';
 
 
 // export default defineConfig({
@@ -15,7 +15,7 @@ export default defineConfig(({ command, mode })=>{
   console.log(command, mode);
   const env = loadEnv(mode, process.cwd())
 
-  console.log(env.VITE_NAME);
+  console.log(env.VITE_NAME);  
 
   if( mode === 'development'){
     return {
@@ -27,11 +27,16 @@ export default defineConfig(({ command, mode })=>{
     console.log('En modo Produccion');
     return {
       build: {
-        rollupOptions: {
-          input:{
-            main: resolve(__dirname, 'index.html'),
-            help: resolve(__dirname, 'help', 'help.html')
-          }
+        // rollupOptions: {
+        //   input:{
+        //     main: resolve(__dirname, 'index.html'),
+        //     help: resolve(__dirname, 'help', 'help.html')
+        //   }
+        // }
+        lib: {
+          entry: resolve(__dirname, 'lib', 'main.js'),
+          name: 'demo',
+          fileName: (format) => `demo.${format}.js`
         }
       }
     }
